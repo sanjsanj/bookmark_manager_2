5979551e5e066ecb5905d0efd3b463e79a6af367
sanjsanj <sanjsanj@hotmail.com>
2015-04-07 17:55:36 +0100

    Let new user sign up

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'tag'
+require 'user'
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -11,6 +12,8 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all
@@ -31,6 +34,22 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(:text => params[:text])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email], :password => params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
   run! if app_file == $PROGRAM_NAME
