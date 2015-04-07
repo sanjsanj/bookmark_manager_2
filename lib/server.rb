e3ac4d0de8660be3521a837fa7d7a6a8a5ab4de8
sanjsanj <sanjsanj@hotmail.com>
2015-04-07 16:41:36 +0100

    Can filter list by tags

--- lib/server.rb
+++ lib/server.rb
@@ -27,5 +27,11 @@ class BookmarkManager < Sinatra::Base
     redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
