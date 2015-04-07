83ef3a7b810e9f03faba8e8d2d8d1e742fc523e3
sanjsanj <sanjsanj@hotmail.com>
2015-04-07 14:06:12 +0100

    Start testing for lists of links

--- lib/server.rb
+++ lib/server.rb
@@ -3,17 +3,11 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-# we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link' # this needs to be done after datamapper is initialised
-
-# After declaring your models, you should finalise them
+require './lib/link'
 DataMapper.finalize
-
-# However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
-
 
 class BookmarkManager < Sinatra::Base
 
@@ -22,5 +16,4 @@ class BookmarkManager < Sinatra::Base
   end
 
   run! if app_file == $PROGRAM_NAME
-
 end
